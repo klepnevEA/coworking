c24fa8bf290606a1e1added6c92eea8783c4be3f
Klepnev <klepnev-ea@yandex.ru>
2018-06-30 17:58:47 +0300

    commit

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+'use strict';
+
+global.$ = {
+  package: require('./package.json'),
+  config: require('./gulp/config'),
+  path: {
+    task: require('./gulp/paths/tasks.js')
+  },
+  gulp: require('gulp'),
+  rimraf: require('del'),
+  merge: require('merge-stream'),
+  browserify : require('browserify'),
+  source : require('vinyl-source-stream'),
+  buffer : require('vinyl-buffer'),
+  babel : require('babelify'),
+  browserSync: require('browser-sync').create(),
+  gp: require('gulp-load-plugins')()
+};
+
+$.path.task.forEach(function(taskPath) {
+  require(taskPath)();
+});
+
+$.gulp.task('default', $.gulp.series(
+  'clean',
+  $.gulp.parallel(
+    'less',
+    'copy:dist',
+    'copy:fonts',
+    'fileinclude',
+    'copy:image',
+    'copy:scripts'
+  ),
+  $.gulp.parallel(
+    'watch',
+    'serve'
+  )
+));
